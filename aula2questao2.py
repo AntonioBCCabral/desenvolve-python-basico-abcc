59e066128bf9993668c8e09a5fbb97771118487c
Benício Cabral <antonio.cabral@pdbomdespacho.com.br>
2025-07-30 10:49:08 -0300

    Add files via upload

---
+++ aula2questao2.py
@@ -0,0 +1,12 @@
+resultado = "O resultado é"
+a = 10
+b = 3.5
+c = a + b
+print(resultado)
+print(a)
+print(b)
+print(c)
+print(type(resultado))
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ aula2questao4.py
@@ -0,0 +1,7 @@
+Juros = 1.01
+Saldo = 500
+Saldo = Saldo * Juros
+Saldo = Saldo * Juros
+Saldo = Saldo * Juros
+print("Seu saldo atual é")
+print(Saldo)
