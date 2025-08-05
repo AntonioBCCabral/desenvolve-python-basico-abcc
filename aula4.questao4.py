e8c8650abaa818fe8c0f556c43062b9e7e78ae51
Benício Cabral <antonio.cabral@pdbomdespacho.com.br>
2025-08-05 11:57:58 -0300

    Add files via upload

--- aula4.questao4.py
+++ aula4.questao4.py
@@ -6,11 +6,11 @@ cedulas_10 = (valor_inserido - (cedulas_100 * 100 + cedulas_50 * 50 + cedulas_20
 cedulas_5 = (valor_inserido - (cedulas_100 * 100 + cedulas_50 * 50 + cedulas_20 * 20 + cedulas_10 * 10)) // 5
 cedulas_2 = (valor_inserido - (cedulas_100 * 100 + cedulas_50 * 50 + cedulas_20 * 20 + cedulas_10 * 10 + cedulas_5 * 5)) // 2
 moedas_1 = valor_inserido - (cedulas_100 * 100 + cedulas_50 * 50 + cedulas_20 * 20 + cedulas_10 * 10 + cedulas_5 * 5 + cedulas_2 * 2)
-print(f"\033[1m{cedulas_100} \033[3m Notas de R$ 100 \033[0m")
-print(f"\033[1m{cedulas_50} \033[3m Notas de R$ 50 \033[0m")
-print(f"\033[1m{cedulas_20} \033[3m Notas de R$ 20 \033[0m")
-print(f"\033[1m{cedulas_10} \033[3m Notas de R$ 10 \033[0m")
-print(f"\033[1m{cedulas_5} \033[3m Notas de R$ 5 \033[0m")
-print(f"\033[1m{cedulas_2} \033[3m Notas de R$ 2 \033[0m")
-print(f"\033[1m{moedas_1} \033[3m Moedas de R$ 1 \033[0m")
+print(f"\033[1m{cedulas_100} \033[3m Nota(s) de R$ 100 \033[0m")
+print(f"\033[1m{cedulas_50} \033[3m Nota(s) de R$ 50 \033[0m")
+print(f"\033[1m{cedulas_20} \033[3m Nota(s) de R$ 20 \033[0m")
+print(f"\033[1m{cedulas_10} \033[3m Nota(s) de R$ 10 \033[0m")
+print(f"\033[1m{cedulas_5} \033[3m Nota(s) de R$ 5 \033[0m")
+print(f"\033[1m{cedulas_2} \033[3m Nota(s) de R$ 2 \033[0m")
+print(f"\033[1m{moedas_1} \033[3m Moeda de R$ 1 \033[0m")
 print("\033[3mObrigado por utilizar nosso sistema de troco!\033[0m")
